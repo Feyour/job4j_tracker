7f4ed36d060bf913db54cfafbdbe1186cc56acc5
feyour <feyour@gmail.com>
2024-07-23 21:47:49 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,35 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+
+    public void printInfo() {
+        System.out.println(active);
+        System.out.println(status);
+        System.out.println(message);
+
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        Error errorPam = new Error(true, 15, "one");
+        Error errorPam2 = new Error(false, 27, "two");
+        Error errorPam3 = new Error(true, 948, "java");
+        errorPam.printInfo();
+        errorPam2.printInfo();
+        errorPam3.printInfo();
+    }
+}
