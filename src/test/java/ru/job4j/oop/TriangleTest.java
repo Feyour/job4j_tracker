16243cfaeea098fa0719524c6271112b10e1ac98
feyour <feyour@gmail.com>
2024-07-25 22:16:42 +0300

    2.1. Рефакторинг - Площадь треугольника. [#122642]

--- src/test/java/ru/job4j/oop/TriangleTest.java
+++ src/test/java/ru/job4j/oop/TriangleTest.java
@@ -15,6 +15,7 @@ public class TriangleTest {
         double expected = 8;
         assertThat(result).isCloseTo(expected, offset(0.001));
     }
+
     @Test
     public void when00and00and01Then1() {
         Point a = new Point(0, 0);
