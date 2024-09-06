31e3e5555b7d11c3feab95eb2663afaa37865361
feyour <feyour@gmail.com>
2024-09-06 16:24:32 +0300

    4.2. Интерфейс Input [#181092 #511921]

--- src/main/java/ru/job4j/polymorphism/Bus.java
+++ src/main/java/ru/job4j/polymorphism/Bus.java
@@ -4,16 +4,17 @@ public class Bus implements Transport {
 
     @Override
     public void move() {
-
+        System.out.println("Автобус начал движение");
     }
 
     @Override
     public void passengers(int count) {
-
+        System.out.println("Количество паасажиров " + count);
     }
 
     @Override
     public int refuel(int fuel) {
-        return 0;
+        int fuelCost = 52;
+        return fuel * fuelCost;
     }
 }
