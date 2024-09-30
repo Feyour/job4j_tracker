6fe52c4b2c15d40a85845bc73aaea284dfef1466
feyour <feyour@gmail.com>
2024-09-30 20:10:33 +0300

    0.5. Иерархия исключений и множественный catch. [#219368 #514088]

--- src/main/java/ru/job4j/ex/UserStore.java
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -13,7 +13,7 @@ public class UserStore {
     }
 
     public static boolean validate(User user) throws UserInvalidException {
-        if (!user.isValid() && user.getUsername().length() <= 3) {
+        if (!user.isValid() || user.getUsername().length() <= 3) {
             throw new UserInvalidException("User with name" + user.getUsername() + " not found.");
         }
         return true;
