026c29411d72e8cd9273b17aeed81019cd2ef608
feyour <feyour@gmail.com>
2024-10-22 17:44:20 +0300

    0. Map, HashMap [#212652 #518473]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,6 +7,9 @@ public class UsageMap {
         HashMap<String, String> map = new HashMap<>();
         map.put("feyour@gmail.com", "Albert Gabaydullin");
         map.put("gab_56@mail.ru", "Arthur Gabaydullin");
+        map.put("gab_56@mail.ru", "Maria Steblevsya");
+        map.put("gab_56@mail.ru", "Masha");
+        map.put("job4j@gamil.com", "Albert Gabaydullin");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " " + value);
