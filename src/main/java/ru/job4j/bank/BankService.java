2acea09ec3d827096b05ce25bfeb7cac3ac175f7
feyour <feyour@gmail.com>
2024-10-22 20:25:34 +0300

    3. Банковские переводы [#10038]

---
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,87 @@
+package ru.job4j.bank;
+
+import ru.job4j.bank.Account;
+import ru.job4j.bank.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+    private final Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        users.putIfAbsent(user, new ArrayList<Account>());
+    }
+
+    public void deleteUser(String passport) {
+        for (User user : users.keySet()) {
+            if (user.getPassport().equals(passport)) {
+                users.remove(user);
+            }
+        }
+    }
+
+    public void addAccount(String passport, Account account) {
+        User user = findByPassport(passport);
+        if (user != null) {
+            List<Account> accounts = users.get(user);
+            if (accounts != null && !accounts.contains(account)) {
+                accounts.add(account);
+            } else {
+                List<Account> newAccounts = new ArrayList<>();
+                newAccounts.add(account);
+                users.put(user, newAccounts);
+            }
+        }
+    }
+
+    public User findByPassport(String passport) {
+        for (User user : users.keySet()) {
+            if (user.getPassport().equals(passport)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        User user = findByPassport(passport);
+        if (user != null) {
+            for (Account account : users.get(user)) {
+                if (account != null) {
+                    if (account.getRequisite().equals(requisite)) {
+                        return account;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public boolean transferMoney(String sourcePassport, String sourceRequisite,
+                                 String destinationPassport, String destinationRequisite,
+                                 double amount) {
+        Account sourceAccount = findByRequisite(sourcePassport, sourceRequisite);
+        if (sourceAccount == null) {
+            return false;
+        }
+
+        Account destinationAccount = findByRequisite(destinationPassport, destinationRequisite);
+        if (destinationAccount == null) {
+            return false;
+        }
+        if (sourceAccount.getBalance() < amount || amount <= 0) {
+            return false;
+        }
+        sourceAccount.setBalance(sourceAccount.getBalance() - amount);
+        destinationAccount.setBalance(destinationAccount.getBalance() + amount);
+        return true;
+
+    }
+
+    public List<Account> getAccounts(User user) {
+        return users.get(user);
+    }
+}
