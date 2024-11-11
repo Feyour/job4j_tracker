31cdf929dbe50f8ed062b1535600e5af7fd58edc
feyour <feyour@gmail.com>
2024-11-11 18:41:01 +0300

    9. Лямбда выражения и Collection Framework [#505026]

--- src/main/java/ru/job4j/hashmap/AnalyzeByMap.java
+++ src/main/java/ru/job4j/hashmap/AnalyzeByMap.java
@@ -1,6 +1,7 @@
 package ru.job4j.hashmap;
 
 import java.util.*;
+import java.util.function.BiFunction;
 
 public class AnalyzeByMap {
     public static double averageScore(List<Pupil> pupils) {
@@ -64,13 +65,12 @@ public class AnalyzeByMap {
     }
 
     public static Label bestSubject(List<Pupil> pupils) {
+        BiFunction<Double, Double, Double> function = (oldValue, newValue) -> oldValue - newValue;
         List<Label> result = new ArrayList<>();
         Map<String, Double> scores = new LinkedHashMap<>();
-        Map<String, Integer> counts = new LinkedHashMap<>();
         for (Pupil pupil : pupils) {
             for (Subject subject : pupil.subjects()) {
-                scores.put(subject.name(), scores.getOrDefault(subject.name(), 0.0) + subject.score());
-                counts.put(subject.name(), counts.getOrDefault(subject.name(), 0) + 1);
+                scores.merge(subject.name(), (double) subject.score(), function);
             }
         }
         for (Map.Entry<String, Double> entry : scores.entrySet()) {
